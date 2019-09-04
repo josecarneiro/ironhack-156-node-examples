eae8372263b77a703912140992e7bdce31524f52
José Carneiro <josecarneiro@Joses-MacBook-Pro.local>
2019-09-04 15:29:08 +0100

    Updated layout and partials example

--- node-layouts-and-partials/index.js
+++ node-layouts-and-partials/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 
 // Create express app
 const app = express();
@@ -7,32 +8,33 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 // Handle Routes
 app.get('/', (req, res, next) => {
-  // res.sendFile(__dirname + '/views/index.html');
   res.render('index');
 });
 
 app.get('/about', (req, res, next) => {
   const data = {
-    name: 'José Carneiro',
-    location: {
-      country: 'Portugal',
-      city: 'Elvas'
-      // region: 'Alentejo'
-    },
-    animals: [
-      'Panda',
-      'Milu',
-      'Whiskers'
-    ],
-    schools: {
-      primary: 'Colégio Luso-Britânico',
-      secondarySchool: 'Escola Secundária D. Sancho II',
-      college: 'CLSBE'
-    }
+    name: 'José Carneiro'
   };
   res.render('about', data);
+});
+
+app.get('/students', (req, res, next) => {
+  const data = {
+    students: [
+      { name: 'Benedita', country: 'Portugal', photo: 'https://source.unsplash.com/1600x900/?student' },
+      { name: 'Filipe', country: 'Portugal', photo: 'https://source.unsplash.com/1600x901/?student' },
+      { name: 'Paula', country: 'Germany', photo: 'https://source.unsplash.com/1600x902/?student' },
+      { name: 'Kidist', country: 'Ethiopia', photo: 'https://source.unsplash.com/1600x903/?student' },
+      { name: 'Gustavo', country: 'Brazil', photo: 'https://source.unsplash.com/1600x904/?student' },
+      { name: 'Sam', country: 'United States of America', photo: 'https://source.unsplash.com/1600x905/?student' },
+      { name: 'Tiago', country: 'Portugal', photo: 'https://source.unsplash.com/1600x906/?student' }
+    ]
+  };
+  res.render('students', data);
 });
 
 // Listen on Port 3000
