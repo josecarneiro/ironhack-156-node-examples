d7f0cb33f862255fb0cd5c5dffa06aca5c42dbc4
José Carneiro <josecarneiro@Joses-MacBook-Pro.local>
2019-09-05 17:40:31 +0100

    Added mongoose examples

--- node-mongoose-introduction/index.js
+++ node-mongoose-introduction/index.js
@@ -6,7 +6,9 @@ const DATABASE_NAME = 'example';
 mongoose.connect(`${ MONGODB_URI }/${ DATABASE_NAME }`, { useNewUrlParser: true })
   .then(message => {
     console.log('Mongoose connected.');
-    executeExample();
+    // createAndListBooks();
+    // loadSingleItem();
+    alternativeDocumentCreation();
   })
   .catch(error => {
     console.log('Error connecting to database');
@@ -15,7 +17,7 @@ mongoose.connect(`${ MONGODB_URI }/${ DATABASE_NAME }`, { useNewUrlParser: true 
 
 const Book = require('./models/book');
 
-function executeExample () {
+function createAndListBooks () {
   const book = new Book({
     title: '1984',
     release: 1947
@@ -28,6 +30,7 @@ function executeExample () {
         .then(books => {
           console.log('Loaded all of the books');
           console.log(books);
+          
         })
         .catch(error => {
           console.log('Error loading books');
@@ -36,4 +39,39 @@ function executeExample () {
     .catch(error => {
       console.log('There was an error saving book.');
     });
+}
+
+function loadSingleItem () {
+  // Book.findOne({ _id: '5d7127d451713e5f0ce33dac' })
+  Book.findById('5d7127d451713e5f0ce33dac')
+    .then(book => {
+      console.log('Found book!');
+      // console.log(book)
+      book.release = 1967;
+      book.save()
+        .then(() => {
+          console.log('Book was successfully saved.');
+        })
+        .catch(error => {
+          console.log('Book was not successfully saved.');
+        });
+      console.log('This piece of code is going to run synchronously');
+    })
+    .catch(error => {
+      console.log('Got an error loading book.');
+    });
+}
+
+function alternativeDocumentCreation () {
+  Book.create({
+    title: 'Hello World',
+    release: 2018
+  })
+    .then(book => {
+      console.log('Created new book');
+      console.log(book);
+    })
+    .catch(error => {
+      console.log('Had an error creating the book');
+    })
 }
--- node-mongoose-introduction/models/book.js
+++ node-mongoose-introduction/models/book.js
@@ -1,8 +1,38 @@
 const mongoose = require('mongoose');
 
+// const bookSchema = new mongoose.Schema({
+//   title: String,
+//   release: Number
+// });
+
 const bookSchema = new mongoose.Schema({
-  title: String,
-  release: Number
+  title: {
+    type: String,
+    minlength: 3,
+    maxlength: 20,
+    lowercase: true,
+    // uppercase: true,
+    required: true,
+    enum: [ '1984', 'Hello World' ],
+    validate: (value) => {
+      const swearWords = [
+        'potato',
+        'carrot',
+        'cheese'
+      ];
+      return !swearWords.includes(value);
+    }
+  },
+  release: {
+    type: Number,
+    min: 1800,
+    max: 2020,
+    default: 2000
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Book = mongoose.model('Book', bookSchema);
