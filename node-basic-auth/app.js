f03fb75fcde5efc3287655b71402aad854d6cb19
José Carneiro <josecarneiro@Joses-MacBook-Pro.local>
2019-09-09 18:15:11 +0100

    Finalized basic authentication example

--- node-basic-auth/app.js
+++ node-basic-auth/app.js
@@ -8,6 +8,10 @@ const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
 
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongo')(expressSession);
+const mongoose = require('mongoose');
+
 const indexRouter = require('./routes/index');
 const authenticationRouter = require('./routes/authentication');
 
@@ -17,10 +21,6 @@ const app = express();
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
 app.use(sassMiddleware({
@@ -29,6 +29,29 @@ app.use(sassMiddleware({
   outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
   sourceMap: true
 }));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 60 * 60 * 24 * 1000 },
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}));
+
+// Custom piece of middleware
+app.use((req, res, next) => {
+  // Access user information from within my templates
+  res.locals.user = req.session.user;
+  // Keep going to the next middleware or route handler
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/authentication', authenticationRouter);
--- node-basic-auth/routes/authentication.js
+++ node-basic-auth/routes/authentication.js
@@ -2,14 +2,20 @@
 
 const { Router } = require('express');
 const router = Router();
+const User = require('./../models/user');
+const bcrypt = require('bcrypt');
+
+const routeGuardMiddleware = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/authentication/sign-in');
+  } else {
+    next();
+  }
+};
 
 router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
-
-const User = require('./../models/user');
-
-const bcrypt = require('bcrypt');
 
 router.post('/sign-up', (req, res, next) => {
   const email = req.body.email;
@@ -23,11 +29,13 @@ router.post('/sign-up', (req, res, next) => {
       });
     })
     .then(user => {
-      // console.log('Signed up user', user);
+      req.session.user = {
+        _id: user._id
+      };
       res.redirect('/authentication/private');
     })
     .catch(error => {
-      console.log('There was an error in the sign up process.');
+      console.log('There was an error in the sign up process.', error);
     });
 });
 
@@ -38,12 +46,60 @@ router.get('/sign-in', (req, res, next) => {
 router.post('/sign-in', (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  console.log(email, password);
-  res.redirect('private');
+  
+  let auxiliaryUser;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        throw new Error('USER_NOT_FOUND');
+      } else {
+        auxiliaryUser = user;
+        return bcrypt.compare(password, user.passwordHash);
+      }
+    })
+    .then(matches => {
+      if (!matches) {
+        throw new Error('PASSWORD_DOESNT_MATCH');
+      } else {
+        req.session.user = {
+          _id: auxiliaryUser._id
+        };
+        res.redirect('private');
+      }
+    })
+    .catch(error => {
+      console.log('There was an error signing up the user', error);
+      next(error);
+    });
 });
 
-router.get('/private', (req, res, next) => {
+/*
+// EXAMPLE WITH ASYNC AWAIT
+// DO NOT USE
+router.post('/sign-in', async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error('USER_NOT_FOUND');
+    const matches = await bcrypt.compare(password, user.passwordHash);
+    if (!matches) throw new Error('PASSWORD_DOESNT_MATCH');
+    req.session.user = { _id: user._id };
+    res.redirect('private');
+  } catch (error) {
+    console.log('There was an error signing up the user', error);
+    next(error);
+  }
+});
+*/
+
+router.get('/private', routeGuardMiddleware, (req, res, next) => {
   res.render('private');
+});
+
+router.post('/sign-out', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
 });
 
 module.exports = router;
