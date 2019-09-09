f91b980342cae531f316bc69746b2b35e82fa960
José Carneiro <josecarneiro@Joses-MacBook-Pro.local>
2019-09-09 16:14:53 +0100

    Basic authentication with account creation

--- node-basic-auth/models/user.js
+++ node-basic-auth/models/user.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// User model goes here
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', schema);
--- node-basic-auth/routes/authentication.js
+++ node-basic-auth/routes/authentication.js
@@ -7,11 +7,28 @@ router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
 
+const User = require('./../models/user');
+
+const bcrypt = require('bcrypt');
+
 router.post('/sign-up', (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  console.log(email, password);
-  res.redirect('private');
+
+  bcrypt.hash(password, 10)
+    .then(hash => {
+      return User.create({
+        email,
+        passwordHash: hash
+      });
+    })
+    .then(user => {
+      // console.log('Signed up user', user);
+      res.redirect('/authentication/private');
+    })
+    .catch(error => {
+      console.log('There was an error in the sign up process.');
+    });
 });
 
 router.get('/sign-in', (req, res, next) => {
